b31b69ef60d55349bb993d897ad91f078a69957b
Fernando Cicconeto <fcicconeto@gmail.com>
2023-02-14 21:47:10 -0300

    Declare student repository

---
+++ src/MusicSchool.SchoolManagement.Domain/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+using MusicSchool.SchoolManagement.Domain.Entities;
+
+namespace MusicSchool.SchoolManagement.Repositories;
+
+public interface IRepository<TEntity> where TEntity : IAggregateRoot
+{
+    Task<TEntity> FindOneAsync(Guid id);
+
+    Task CreateAsync(TEntity entity);
+}
---
+++ src/MusicSchool.SchoolManagement.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using MusicSchool.SchoolManagement.Domain.Entities;
+using MusicSchool.SchoolManagement.Repositories;
+
+namespace MusicSchool.SchoolManagement.Infrastructure.Repositories;
+
+public class StudentRepository : IRepository<Student>
+{
+    public Task CreateAsync(Student entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Student> FindOneAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
