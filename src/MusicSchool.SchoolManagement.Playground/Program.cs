1c13bea67927010866702f5a2bbd528fd1c29347
Fernando Cicconeto <fcicconeto@gmail.com>
2023-02-14 21:56:56 -0300

    Add playground app

---
+++ src/MusicSchool.SchoolManagement.Playground/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MusicSchool.SchoolManagement.Domain.Entities;
+using MusicSchool.SchoolManagement.Infrastructure.Repositories;
+
+StudentRepository studentRepo = new();
+
+Student student = new()
+{
+    Id = Guid.NewGuid(),
+    Name = "Luiz Melodia",
+};
+
+await studentRepo.CreateAsync(student);
+
+Student persistedStudent = await studentRepo.FindOneAsync(student.Id);
+
+Console.WriteLine("Information about persisted student:");
+Console.WriteLine($"ID   = \"{persistedStudent.Id}\"");
+Console.WriteLine($"Name = \"{persistedStudent.Name}\"");
