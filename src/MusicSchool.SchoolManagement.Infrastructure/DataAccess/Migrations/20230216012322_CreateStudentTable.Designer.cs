679c9c3740f4ae9661003682dbf85565e343b938
Fernando Cicconeto <fcicconeto@gmail.com>
2023-02-15 22:25:28 -0300

    Fix package compatibility

--- src/MusicSchool.SchoolManagement.Infrastructure/DataAccess/Migrations/20230216012322_CreateStudentTable.Designer.cs
+++ src/MusicSchool.SchoolManagement.Infrastructure/DataAccess/Migrations/20230216012322_CreateStudentTable.Designer.cs
@@ -11,7 +11,7 @@ using MusicSchool.SchoolManagement.Infrastructure.DataAccess;
 namespace MusicSchool.SchoolManagement.Infrastructure.DataAccess.Migrations
 {
     [DbContext(typeof(SchoolManagementContext))]
-    [Migration("20230216011525_CreateStudentTable")]
+    [Migration("20230216012322_CreateStudentTable")]
     partial class CreateStudentTable
     {
         /// <inheritdoc />
@@ -19,7 +19,7 @@ namespace MusicSchool.SchoolManagement.Infrastructure.DataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.3")
+                .HasAnnotation("ProductVersion", "7.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("MusicSchool.SchoolManagement.Domain.Entities.Student", b =>
--- src/MusicSchool.SchoolManagement.Infrastructure/DataAccess/Migrations/SchoolManagementContextModelSnapshot.cs
+++ src/MusicSchool.SchoolManagement.Infrastructure/DataAccess/Migrations/SchoolManagementContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace MusicSchool.SchoolManagement.Infrastructure.DataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.3")
+                .HasAnnotation("ProductVersion", "7.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("MusicSchool.SchoolManagement.Domain.Entities.Student", b =>
