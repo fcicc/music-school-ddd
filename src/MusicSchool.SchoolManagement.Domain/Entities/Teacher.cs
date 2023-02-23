dc1ee9cd62b3243eaccbf39dfd859db230d78cbb
Fernando Cicconeto <fcicconeto@gmail.com>
2023-02-23 08:52:26 -0300

    Remove allocation and teacher entities

--- src/MusicSchool.SchoolManagement.Domain/Entities/Teacher.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicSchool.SchoolManagement.Domain.Entities;
-
-public class Teacher : IAggregateRoot
-{
-    public Guid Id { get; init; }
-
-    public string Name { get; init; } = "";
-}
