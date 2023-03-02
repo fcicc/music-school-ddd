19be2bf9b90d0f04f3c260fe2165d1a61fad08c8
Fernando Cicconeto <fcicconeto@gmail.com>
2023-03-02 20:05:49 -0300

    Revert "Add school management client"
    This reverts commit e763a9917e5a27158da2ce0e7581d9be2a9a77d3.

--- src/MusicSchool.SchoolManagement.Client/Models/Response/Course.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicSchool.SchoolManagement.Client.Models.Response;
-
-public class Course
-{
-    public Guid Id { get; init; }
-
-    public string Name { get; init; } = "";
-}
--- src/MusicSchool.SchoolManagement.Client/Models/Response/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicSchool.SchoolManagement.Client.Models.Response;
-
-public class Student
-{
-    public Guid Id { get; init; }
-
-    public string Name { get; init; } = "";
-}
