ac4cd5e78f87e9aadbf8321c686092c546ff50bb
Fernando Cicconeto <fcicconeto@gmail.com>
2023-03-06 21:37:14 -0300

    Work around possible bug in ASP.NET JSON serialization

--- src/MusicSchool.Finance.Domain/Services/ITransactionService.cs
+++ src/MusicSchool.Finance.Domain/Services/ITransactionService.cs
@@ -18,11 +18,15 @@ public interface ITransactionService
         public BrlAmount Value { get; init; }
     }
 
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+    [JsonDerivedType(typeof(CreateExtraPaymentRequest), "ExtraPayment")]
     public class CreateExtraPaymentRequest : CreateTransactionRequest
     {
         public string Description { get; init; } = "";
     }
 
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+    [JsonDerivedType(typeof(CreateInvoicePaymentRequest), "InvoicePayment")]
     public class CreateInvoicePaymentRequest : CreateTransactionRequest
     {
         public Guid InvoiceId { get; init; }
