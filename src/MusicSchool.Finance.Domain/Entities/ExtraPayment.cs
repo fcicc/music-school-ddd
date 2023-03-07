6b740579767344a3e3a866b9ace48a90a4972cc4
Fernando Cicconeto <fcicconeto@gmail.com>
2023-03-06 21:36:11 -0300

    Work around possible bug in asp.net JSON serialization

--- src/MusicSchool.Finance.Domain/Entities/ExtraPayment.cs
+++ src/MusicSchool.Finance.Domain/Entities/ExtraPayment.cs
@@ -1,5 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace MusicSchool.Finance.Domain.Entities;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(ExtraPayment), "ExtraPayment")]
 public class ExtraPayment : Transaction
 {
     internal ExtraPayment() { }
--- src/MusicSchool.Finance.Domain/Entities/InvoicePayment.cs
+++ src/MusicSchool.Finance.Domain/Entities/InvoicePayment.cs
@@ -1,5 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace MusicSchool.Finance.Domain.Entities;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(InvoicePayment), "InvoicePayment")]
 public class InvoicePayment : Transaction
 {
     internal InvoicePayment() { }
